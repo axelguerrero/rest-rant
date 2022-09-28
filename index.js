bd815889c4fdeb942805976c6ef7d98acde359ab
axelguerrero <guerreroaxel230@yahoo.com>
2022-09-28 17:54:15 -0400

    adding homepage view

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const express = require("express")
 const app = express()
 
 //added code to import the router
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use("/places", require("./controllers/places"))
 
 //home page route
-app.get ("/", (req, res) => {
-        res.send("Yur!")
+app.get("/", (req, res) => {
+        res.render("home")
 })
 
-app.get ("*", (req, res) => {
+//error page route
+app.get("*", (req, res) => {
         res.status(404).send("<h1>404 Page</h1>")
 })
 
