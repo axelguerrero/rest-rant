a9e341f8f86d985bf94a9dd647985984bc2d190e
axelguerrero <guerreroaxel230@yahoo.com>
2022-09-30 20:07:22 -0400

    adding styles/css

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 
-//added code to import the router
+//added code to import the router, express settings
+app.set("views", __dirname + "/views")
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
 app.use("/places", require("./controllers/places"))
+app.use(express.static("public"))
 
 //home page route
 app.get("/", (req, res) => {
@@ -19,4 +20,5 @@ app.get('*', (req, res) => {
         res.render('error404')
       })      
 
+      //listen for connections
 app.listen(process.env.PORT)
