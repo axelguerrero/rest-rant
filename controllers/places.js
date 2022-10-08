5514d5a7ff61d03e3029161daae17e624b00f6f8
axelguerrero <guerreroaxel230@yahoo.com>
2022-10-08 11:41:49 -0400

    show route

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const places = require("../models/places.js")
 
+//new route
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
-
+//new post route
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
@@ -18,10 +19,27 @@ router.post('/', (req, res) => {
     req.body.state = 'USA'
   }
   places.push(req.body)
-  res.redirect("/places")
+  res.redirect('/places')
+})
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render("error404")
+  }
+
+  else {
+    res.render('places/show', { place: places[id] })
+
+  }
 })
 
 
+
+//data
 router.get("/", (req, res) => {
   let places = [{
     name: 'H-Thai-ML',
--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const app = express()
 app.set("views", __dirname + "/views")
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.urlencoded({ extended: true }))
 app.use("/places", require("./controllers/places"))
 app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }))
+
 
 //home page route
 app.get("/", (req, res) => {
